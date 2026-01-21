c3015ee145e41140266cfaee4a0f6f9a30c35970
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-01-22 00:47:49 +0530

    Handel API error and response

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ dotenv.config({
   path: "./.env",
 });
 
+const PORT = process.env.PORT || 8000;
+
 // Connect to Database
 connectDB()
-  .then(() => {})
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
   .catch((err) => {
     console.error("MongoDB connection error", err);
     process.exit(1);
   });
-
-const PORT = process.env.PORT || 8000;
