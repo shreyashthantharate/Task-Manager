ef7115b321e1c59da5f045091d1d65661e078874
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-01-22 00:18:07 +0530

    Setup MongoDB connection created

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
 dotenv.config({
   path: "./.env",
 });
+
+// Connect to Database
+connectDB()
+  .then(() => {})
+  .catch((err) => {
+    console.error("MongoDB connection error", err);
+    process.exit(1);
+  });
 
 const PORT = process.env.PORT || 8000;
