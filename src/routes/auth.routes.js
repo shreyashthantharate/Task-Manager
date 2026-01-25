9bbff00d3a32c3c5d32fe5511d61b7dec67f7efa
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-01-26 00:20:52 +0530

    use our userRegistrationValidator in routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,11 @@
-import e from "cors";
 import { Router } from "express";
+import { registerUser } from "../controllers/auth.controllers.js";
+import { userRegistrationValidator } from "../validators/index.js";
+import { validate } from "../middlewares/validator.middleware.js";
 
 const router = Router();
+router
+    .route("/register")
+    .post(userRegistrationValidator(), validate, registerUser);
 
 export default router;
