191b7f83365871aaf668ef79d0edf94485a30b3e
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-01-27 00:07:26 +0530

    Fix imports in controllers and app file

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 app.use(express.json({ limit: "16kb" }));
--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,5 +1,5 @@
 import { asyncHandler } from "../utils/async-handler.js";
-import User from "../models/user.models.js";
+import { User } from "../models/user.models.js";
 import ApiError from "../utils/api-error.js";
 import ApiResponse from "../utils/api-response.js";
 
