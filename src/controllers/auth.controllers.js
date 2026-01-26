300deaab93bfd91b86498889a0bdb0cc954d693e
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-01-26 17:35:55 +0530

    Add all controllers boiler plate

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -5,4 +5,20 @@ const registerUser = asyncHandler(async (req, res) => {
     userRegistrationValidator(body);
 });
 
+const loginUser = asyncHandler(async (req, res) => {});
+
+const logoutUser = asyncHandler(async (req, res) => {});
+
+const verifyEmail = asyncHandler(async (req, res) => {});
+
+const resendVerificationEmail = asyncHandler(async (req, res) => {});
+
+const refreshAccessToken = asyncHandler(async (req, res) => {});
+
+const forgotPasswordRequest = asyncHandler(async (req, res) => {});
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {});
+
+const getCurrentUser = asyncHandler(async (req, res) => {});
+
 export { registerUser };
