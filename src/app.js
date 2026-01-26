2b0991ffa1e44feb85926473b741872f17ade16b
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-01-27 00:01:29 +0530

    configure app baisc setting and import auth router

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
 import express from "express";
 
 const app = express();
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public")); // configure static file to save images locally
+app.use(cookieParser());
+
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:8000",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    }),
+);
 
 // router imports
 import healthCheckRouter from "./routes/healthcheck.routes.js";
+import authRouter from "./routes/auth.routes.js";
 
 app.use("/api/v1/healthcheck", healthCheckRouter);
+app.use("/api/v1/auth", authRouter);
 
 export default app;
